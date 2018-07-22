15dbc84b64b4d5ae879395d4ad7d7c12ba1590c0
Blendan <37484137+Blendan@users.noreply.github.com>
2018-07-22 21:59:05 +0200

    neuse css,js und desing

--- Onlineauftrit2/js/master.js
+++ Onlineauftrit2/js/master.js
@@ -1,10 +1,44 @@
 
 "use strict";
-var name_text = false;
 
 $(document).ready(
   function()
   {
-    tippenAnimaton();
+    setInterval(drehenHaupt,3000);
   }
 );
+
+var dehen_aktuell = 0;
+
+function drehenHaupt()
+{
+  var haupt_text = document.getElementsByClassName("haupt_ueberschrift");
+
+  $(".rotateOut").addClass("rotadeHidden");
+  $(".rotateOut").removeClass("rotateOut");
+
+  for (var i = 0; i < haupt_text.length; i++)
+  {
+    //alert(i==(dehen_aktuell-1)&&i!=0);
+    if (i==dehen_aktuell)
+    {
+      $(haupt_text[i]).addClass("rotateIN");
+      $(haupt_text[i]).removeClass("rotadeHidden");
+    }
+    else if (i==(dehen_aktuell-1)&&i!=(haupt_text.length-1)||0==dehen_aktuell&&i==(haupt_text.length-1))
+    {
+      $(haupt_text[i]).addClass("rotateOut");
+      $(haupt_text[i]).removeClass("rotateIN");
+    }
+    else
+    {
+      $(haupt_text[i]).addClass("rotadeHidden");
+      $(haupt_text[i]).removeClass("rotateIN");
+    }
+  }
+  dehen_aktuell ++;
+  if (dehen_aktuell==haupt_text.length)
+  {
+    dehen_aktuell = 0;
+  }
+}
