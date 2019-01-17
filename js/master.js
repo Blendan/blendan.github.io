8768b8764a8ddb47f089a0d7fa3506ea664ae116
Blendan <37484137+Blendan@users.noreply.github.com>
2019-01-17 18:12:31 +0100

    js fehler gefixt

--- js/master.js
+++ js/master.js
@@ -3,6 +3,7 @@
 $( document ).ready(
     function()
     {
+      $('hintergrund').css('background-position', 'center ' + (wScroll*-0.1) + 'px');
       $(window).scroll(
           function()
           {
